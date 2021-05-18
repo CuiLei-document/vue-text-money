17d8744de83a2435261d3504a2e861e421deaee0
磊哥 <493114008@qq.com>
2021-05-18 11:45:57 +0800

    deploy

---
+++ precache-manifest.35e227f2a6b483bf7a63f830d68fbe85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13ec5cac82d47a989e9",
+    "url": "/vue-text-money/css/app.d311c1d1.css"
+  },
+  {
+    "revision": "53be9cca5bc58ea83268c9477500c6b0",
+    "url": "/vue-text-money/index.html"
+  },
+  {
+    "revision": "a13ec5cac82d47a989e9",
+    "url": "/vue-text-money/js/app.8e00d5c7.js"
+  },
+  {
+    "revision": "bc28f30c4e0b75639d2a",
+    "url": "/vue-text-money/js/chunk-vendors.e2b3ae6d.js"
+  },
+  {
+    "revision": "984f607da7be3d4ce30439fc42288f97",
+    "url": "/vue-text-money/manifest.json"
+  },
+  {
+    "revision": "5bc24b2f0e5b0743fe8dce71a8f9f18e",
+    "url": "/vue-text-money/qmoney.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-text-money/robots.txt"
+  }
+]);
